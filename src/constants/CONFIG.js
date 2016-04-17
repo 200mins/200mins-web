354e74cf38a1febeaf88c68f76dde56b86f6ad0f
karmasakshi <karmasakshi@outlook.com>
2016-04-17 23:29:22 +0530

    Add deployment config.

--- src/constants/CONFIG.js
+++ src/constants/CONFIG.js
@@ -4,12 +4,12 @@ angular.module('200mins-web').constant('CONFIG', {
 
     DEV: {
         domain: 'http://localhost',
-        port: 1337
+        port: 1339
     },
 
     PROD: {
         domain: 'http://200mins.com',
-        port: 80
+        port: 1339
     }
 
 });
