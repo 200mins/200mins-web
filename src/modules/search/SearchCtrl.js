9ca7f797544fdecec430d07ce5aa933cf08d5a16
karmasakshi <karmasakshi@outlook.com>
2016-05-12 00:44:58 +0530

    Search fix.

--- src/modules/search/SearchCtrl.js
+++ src/modules/search/SearchCtrl.js
@@ -1,11 +1,12 @@
 'use strict';
 
-angular.module('200mins-web').controller('SearchCtrl', ['$rootScope', '$scope', '$state', '$stateParams', '$timeout', 'proxyService', 'utilityService', function ($rootScope, $scope, $state, $stateParams, $timeout, proxyService, utilityService) {
+angular.module('200mins-web').controller('SearchCtrl', ['$rootScope', '$scope', '$state', '$stateParams', '$timeout', 'activityService', 'proxyService', 'utilityService', function ($rootScope, $scope, $state, $stateParams, $timeout, activityService, proxyService, utilityService) {
 
         /* --- Models --- */
 
         // $scope.isSearchActive;
         // $scope.query;
+        // $scope.selectedMovie;
         // $scope.suggestions;
         // $scope.timeout;
 
@@ -32,8 +33,62 @@ angular.module('200mins-web').controller('SearchCtrl', ['$rootScope', '$scope', 
                 $scope.timeout = 2000;
 
             }
+            
+            $scope.selectedMovie = {id: null, intention: null};
 
             $scope.suggestions = [];
+
+        };
+        
+        $scope.movieDownload = function (imdbID, torrent, e) {
+
+            $rootScope.initializeUser().then(function () {
+
+                activityService.postMovieDownload(imdbID, torrent.quality).then(function (response) {
+
+                    if (typeof response === 'undefined') {
+
+                        utilityService.notify('Couldn\'t reach 200mins.');
+
+                    } else {
+
+                        switch (response.status) {
+
+                            case 200:
+
+                                $rootScope.changeURL(torrent.url, false);
+
+                                break;
+
+                            case 403:
+
+                                utilityService.notify(response.data);
+
+                                break;
+
+                            case 401:
+
+                                $rootScope.logout(true);
+
+                                utilityService.notify(response.data);
+
+                                break;
+
+                            default:
+
+                                utilityService.notify('Service is down.');
+
+                        }
+
+                    }
+
+                });
+
+            }, function () {
+
+                $rootScope.showLoginDialog(e);
+
+            });
 
         };
 
@@ -94,6 +149,75 @@ angular.module('200mins-web').controller('SearchCtrl', ['$rootScope', '$scope', 
         $scope.showMovieDetails = function (movieID) {
 
             $rootScope.changeURL($state.href('movie', {id: movieID}), true);
+
+        };
+        
+        $scope.resetSelectedMovie = function () {
+
+            $scope.selectedMovie = {id: null, intention: null};
+
+        };
+
+        $scope.selectMovie = function (id, intention) {
+
+            $scope.selectedMovie = {
+                id: id,
+                intention: intention
+            };
+
+        };
+
+        $scope.moviePlay = function (movie, torrent, e) {
+
+            $rootScope.initializeUser().then(function () {
+
+                activityService.postMoviePlay(movie.imdb_code, torrent.quality).then(function (response) {
+
+                    if (typeof response === 'undefined') {
+
+                        utilityService.notify('Couldn\'t reach 200mins.');
+
+                    } else {
+
+                        switch (response.status) {
+
+                            case 200:
+
+                                var magnetURL = 'magnet:?xt=urn:btih:' + torrent.hash + '&dn=' + encodeURIComponent(movie.title_long) + '&tr=udp://tracker.openbittorrent.com:80&tr=udp://torrent.gresille.org:80/announce';
+
+                                $rootScope.changeURL(magnetURL, false);
+
+                                break;
+
+                            case 403:
+
+                                utilityService.notify(response.data);
+
+                                break;
+
+                            case 401:
+
+                                $rootScope.logout(true);
+
+                                utilityService.notify(response.data);
+
+                                break;
+
+                            default:
+
+                                utilityService.notify('Service is down.');
+
+                        }
+
+                    }
+
+                });
+
+            }, function () {
+
+                $rootScope.showLoginDialog(e);
+
+            });
 
         };
 
